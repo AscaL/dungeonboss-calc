53b93693e150025722c55cccb4478dee1af6cbc1
AscaL <emanueleorfanelli@gmail.com>
2015-10-12 18:15:46 +0200

    update formatting

--- api/models/Hero.js
+++ api/models/Hero.js
@@ -118,35 +118,34 @@ module.exports = {
 		sails.log.verbose('ascensions:', ascensions);
 
 		Hero.findOne({
-				name: hero
-			})
-			.then(function (heroFullStats) {
-				var heroStats = _.pick(heroFullStats, ['attack', 'defense', 'skill', 'health']);
-				sails.log.verbose('heroStats:', heroStats);
+			name: hero
+		})
+		.then(function (heroFullStats) {
+			var heroStats = _.pick(heroFullStats, ['attack', 'defense', 'skill', 'health']);
+			sails.log.verbose('heroStats:', heroStats);
 
-				var newHeroStats = {};
-				newHeroStats.name = hero;
+			var newHeroStats = {};
+			newHeroStats.name = hero;
 
-				_.forOwn(heroStats, function (statValue, statName) {
-					sails.log.verbose('stat:', statName, statValue);
-					var a = ((level - 1) * 0.08) + 1;
-					sails.log.verbose('a:', a);
-					var b = statValue * starPower(stars);
-					sails.log.verbose('b:', b);
-					var c = b * ascPower(ascensions, statName);
-					sails.log.verbose('c:', c);
-					var newStat = _.round(c * a, 1);
+			_.forOwn(heroStats, function (statValue, statName) {
+				sails.log.verbose('stat:', statName, statValue);
+				var a = ((level - 1) * 0.08) + 1;
+				sails.log.verbose('a:', a);
+				var b = statValue * starPower(stars);
+				sails.log.verbose('b:', b);
+				var c = b * ascPower(ascensions, statName);
+				sails.log.verbose('c:', c);
+				var newStat = _.round(c * a, 1);
 
-					sails.log.verbose('newStat:', newStat);
-					newHeroStats[statName] = newStat;
-				})
-				sails.log.verbose('newHeroStats:', newHeroStats);
-				return cb(null, newHeroStats);
-
-			})
-			.catch(function (err) {
-				return cb(err)
-			})
+				sails.log.verbose('newStat:', newStat);
+				newHeroStats[statName] = newStat;
+			});
+			sails.log.verbose('newHeroStats:', newHeroStats);
+			return cb(null, newHeroStats);
+		})
+		.catch(function (err) {
+			return cb(err);
+		});
 	}
 
 };
@@ -160,65 +159,63 @@ function starPower(star) {
 		3: 1.25,
 		4: 1.375,
 		5: 1.5
-	}
+	};
 	sails.log.verbose('starVal:', starVal);
 	sails.log.verbose('star:', star);
 
-	sails.log.verbose('get starval, star:', _.get(starVal, star));
+	sails.log.verbose('get star val, star:', _.get(starVal, star));
 
 	return _.get(starVal, star);
 }
 
 function ascPower(asc, statName) {
-	var currentStats = statName;
 	sails.log.verbose('statName:', statName);
 
 	var ascAttackVal = {
 		name: 'attack',
 		0: 1,
 		1: 1.088,
-		2: 1.186,
-	}
+		2: 1.186
+	};
 
 	var ascDefenseVal = {
 		name: 'defense',
 		0: 1,
 		1: 1.0805,
-		2: 1.177,
-	}
+		2: 1.177
+	};
 
 	var ascSkillVal = {
 		name: 'skill',
 		0: 1,
 		1: 1.0805,
-		2: 1.177,
-	}
+		2: 1.177
+	};
 
 	var ascHealthVal = {
 		name: 'health',
 		0: 1,
 		1: 1.082,
-		2: 1.179,
-	}
+		2: 1.179
+	};
 
 	var ascVal = {
 		ascAttackVal: ascAttackVal,
 		ascDefenseVal: ascDefenseVal,
 		ascSkillVal: ascSkillVal,
 		ascHealthVal: ascHealthVal
-	}
+	};
 
 	var finalValue = null;
 	_.forEach(ascVal, function (ascObj) {
 		sails.log.verbose('ascObj:', ascObj);
 		if (_.isEqual(ascObj.name, statName)) {
 			sails.log.verbose('_get(ascObj, asc):', _.get(ascObj, asc));
-			finalValue = _.get(ascObj, asc)
+			finalValue = _.get(ascObj, asc);
 		}
-	})
+	});
 	sails.log.verbose('finalValue:', finalValue);
-
-
 	sails.log.verbose('get starval, star:', _.get(ascVal, asc));
+
 	return finalValue;
 }
